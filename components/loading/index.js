587ab3260b35e265d06b7195d3e14221869392e4
qihuarui <qihuarui@xiaomi.com>
2019-08-15 16:26:37 +0800

    福利页面下拉刷新

---
+++ components/loading/index.js
@@ -0,0 +1,10 @@
+Component({
+  data: {
+    list: [
+      {
+        name: '餐饮美食',
+        img: 'https://gtms03.alicdn.com/tps/i3/TB1rOnbIFXXXXceXpXXrywt4VXX-120-120.png'
+      }
+    ]
+  }
+})
--- pages/welfare/index.js
+++ pages/welfare/index.js
@@ -3,7 +3,33 @@ const util = require('../../utils/util.js')
 
 Page({
   data: {
-    logs: []
+    list: [
+      {
+        name: '',
+        status: ''
+      },
+      {
+        name: '',
+        status: ''
+      },
+      {
+        name: '',
+        status: ''
+      },
+      {
+        name: '',
+        status: ''
+      },
+      {
+        name: '',
+        status: ''
+      },
+      {
+        name: '',
+        status: ''
+      }
+    ],
+    showLoading: false
   },
   onShow: function () {
     if (typeof this.getTabBar === 'function' &&
@@ -19,6 +45,44 @@ Page({
       //   return util.formatTime(new Date(log))
       // })
     })
-    
+
+  },
+
+  handleClick () {
+    console.log('123')
+    wx.pageScrollTo({
+      scrollTop: 30,
+      duration: 300
+    })
+  },
+  onReachBottom () {
+    this.setData({
+      showLoading: true
+    })
+    console.log('下拉到最下面了~')
+  },
+
+  handleScroll () {
+    this.setData({
+      showLoading: true
+    })
+
+    // 线上环境接口需要进行配置
+    wx.request({
+      url: 'http://www.zhangdachun.vip/dbjb/user/code2Session',
+      data: {
+        code: '111'
+      },
+      method: 'post',
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success (res) {
+        console.log(res)
+      },
+      fail (error) {
+        console.log(error)
+      }
+    })
   }
 })
